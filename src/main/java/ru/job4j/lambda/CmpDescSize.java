4cbc39b45b1feac6fb8878e84b1ce2ad03ccaa95
evgenshegai <evgeniishegai@yandex.ru>
2021-01-18 18:36:34 +1000

    2.2. Лямбда блок [#304243]

--- src/main/java/ru/job4j/lambda/CmpDescSize.java
+++ src/main/java/ru/job4j/lambda/CmpDescSize.java
@@ -7,7 +7,7 @@ public class CmpDescSize  {
     public static void main(String[] args) {
         Comparator<String> comparator = (o1, o2) -> {
             System.out.println(" ReverseCompare " + o2.length() + " : " + o1.length());
-            return Integer.compare(Integer.parseInt(o2), Integer.parseInt(o1));
+            return Integer.compare(o2.length(), o1.length());
         };
     }
 }
