2320ae8c21b187c838c85a0850b133b71d97bc14
evgenshegai <evgeniishegai@yandex.ru>
2021-01-19 18:24:04 +1000

    4. Преобразование List в Map. [#304253

---
+++ src/main/java/ru/job4j/collection/streamapi/ListConvertMap.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection.streamapi;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListConvertMap {
+
+    public Map<String, Class<? extends Student>> covert(List<Student> students) {
+        return  students.stream()
+                .collect(Collectors.toMap(Student::getSurname, Student ::getClass));
+    }
+}
---
+++ src/test/java/ru/job4j/collection/streamapi/ListConvertMapTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection.streamapi;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ListConvertMapTest {
+
+    private List<Student> list = Arrays.asList(new Student(1, "Boris"), new Student(2, "Pavel"),
+    new Student(2, "Sergey"));
+
+    @Test
+    public void whenConvertListInMap() {
+        ListConvertMap convert = new ListConvertMap();
+        Map<String, Class<? extends Student>> result = convert.covert(list);
+        assertThat(result.size(), is(3));
+    }
+}
