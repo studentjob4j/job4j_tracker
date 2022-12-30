50e0c014bea7d5c489bb17645f3df6d3bbb6e4d8
Evgenii <job4jstudy@yandex.ru>
2022-12-30 23:15:52 +1000

    Singleton

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+
+/**
+ *  Singleton
+ * @author Shegai Evgenii
+ * @version 1.0
+ */
+
+public final class SingleTracker {
+
+    private static SingleTracker singleTracker = null;
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (singleTracker == null) {
+            singleTracker = new SingleTracker();
+        }
+        return singleTracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+       return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+       return tracker.replace(id, item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+      return findByName(key);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
