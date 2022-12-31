c2210ccf899c080cca8ba175484ad5e14706fffb
Evgenii <job4jstudy@yandex.ru>
2022-12-31 23:47:34 +1000

    Идентичные тексты

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+/**
+ * Идентичные тексты
+ * @author Shegai Evgenii
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class UniqueText {
+
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (String word : text) {
+            if (!check.contains(word)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
