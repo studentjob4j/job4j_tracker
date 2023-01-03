6a7f781536795a91d99e42a18ff8c17d642a8489
Evgenii <job4jstudy@yandex.ru>
2023-01-04 07:15:51 +1000

    Генерирование колоды карт. FlatMap

---
+++ src/main/java/ru/job4j/stream/CartGenerator.java
@@ -0,0 +1,43 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+/**
+ * Генерирование колоды карт.
+ * FlatMap
+ * @author Shegai Evgenii
+ * @version 1.0
+ * @since 04.01.2023
+ */
+
+public class CartGenerator {
+
+    public enum Suit {
+        Diamonds, Hearts, Spades, Clubs
+    }
+
+    public enum Value {
+        V_6, V_7, V_8
+    }
+
+    public static class Card {
+        private Suit suit;
+        private Value value;
+
+        public Card(Suit suit, Value value) {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Card{" + "suit=" + suit + ", value=" + value + '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values()).map(value -> new Card(suit, value)))
+                        .forEach(System.out::println);
+    }
+}
