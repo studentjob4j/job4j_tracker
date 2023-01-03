0e54b325153cefbf1963344cd69537eba4a8ce18
Evgenii <job4jstudy@yandex.ru>
2023-01-04 03:54:05 +1000

    Отладка лямбда в IDEA

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,36 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+/**
+ *  Отладка лямбда в IDEA
+ * @author Shegai Evgenii
+ * @version 1.0
+ * @since 04.01.2023
+ */
+
+public class StreamUsage {
+
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
+    }
+}
