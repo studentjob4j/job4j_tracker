f525514724f08c6a29dcd2c48828d2e6464d1392
Evgenii <job4jstudy@yandex.ru>
2023-01-04 05:14:29 +1000

    Уникальность элементов и сортировка

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,10 +1,12 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Список адресов
+ * Уникальность элементов и сортировка
  * @author Shegai Evgenii
  * @version 1.0
  * @since 04.01.2023
@@ -14,5 +16,13 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(x -> x.getAddress())
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
