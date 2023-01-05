86448a958a506ae23027556f8b0e36a5db611a41
Evgenii <job4jstudy@yandex.ru>
2023-01-06 06:02:31 +1000

    Local-Variable Type Inference Var

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -26,7 +26,7 @@ public class PhoneDictionary {
         Predicate<Person> address = x -> x.getAddress().equals(key);
         Predicate<Person> combine = name.or(surname.or(phone.or(address)));
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
--- src/test/java/ru/job4j/collection/LexSortTest.java
+++ src/test/java/ru/job4j/collection/LexSortTest.java
@@ -2,6 +2,8 @@ package ru.job4j.collection;
 
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LexSortTest {
